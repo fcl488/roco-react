60cf7fe6ebef53465600aae0aeead7d50985bb0e
fcl <223@qq.com>
2024-10-09 14:35:37 +0800

    用户注册功能 修改icon

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,10 +1,12 @@
 import request from '@/utils/request'
-import type { BaseResult } from '../commonType'
-import type { LoginDTO, LoginVO } from './type'
+import type { BaseResult, CommonRes } from '../commonType'
+import type { LoginDTO, LoginVO, GetVerifyCodeDTO, RegisterDTO } from './type'
 
 enum API {
   GET_PUBLIC_KEY = '/user/get/publickey',
-  LOGIN = '/web/roco/user/login'
+  LOGIN = '/web/roco/user/login',
+  GET_VERIFY_CODE = '/web/roco/user/sendverifycode',
+  REGISTER = '/web/roco/user/register'
 }
 
 export default {
@@ -13,5 +15,11 @@ export default {
   },
   login(params: LoginDTO) {
     return request.post<any, BaseResult<LoginVO>>(API.LOGIN, params)
+  },
+  getVerifyCode(params: GetVerifyCodeDTO) {
+    return request.post<any, CommonRes>(API.GET_VERIFY_CODE, params)
+  },
+  register(params: RegisterDTO) {
+    return request.post<any, CommonRes>(API.REGISTER, params)
   }
 }
